561c24a5e12a511885827598c29de37aaf7bd821
14409 <1440905736@qq.com>
2023-02-12 19:40:20 +0800

    first commit

---
+++ src/main/java/com/cyz/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.cyz.git;
+
+/**
+ * @description: git test
+ * @author: Chan
+ * @create: 2023-02-12 19:29
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

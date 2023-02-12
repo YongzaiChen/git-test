07e2e78aeaa199f27d30839a4f47358a0f3bc9c6
14409 <1440905736@qq.com>
2023-02-12 19:43:53 +0800

    first commit

--- src/main/java/com/cyz/git/GitTest.java
+++ src/main/java/com/cyz/git/GitTest.java
@@ -7,6 +7,6 @@ package com.cyz.git;
  **/
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
